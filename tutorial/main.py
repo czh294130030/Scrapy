a76479b51d43520e0c5df5b0e8838a6a29a25cc5
Chad.Cao <Chad.Cao@movit-tech.com>
2020-05-17 21:46:10 +0800

    # 用户调试Scrapy使用 # Edit Configurations->Python interpreter->python 3.8 # 设置断点后回到 main.py 进行 debug，会自动跳到设置断点处

--- tutorial/main.py
+++ tutorial/main.py
@@ -3,5 +3,7 @@ import os
 import sys
 
 # 用户调试Scrapy使用
+# Edit Configurations->Python interpreter->python 3.8
+# 设置断点后回到 main.py 进行 debug，会自动跳到设置断点处
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(['scrapy','crawl','selenium_spider'])
